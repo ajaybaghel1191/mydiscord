91ee89d1ad172a980b0ededfe1ad28d1ddf2cdc3
ajaybaghel1191 <109842877+ajaybaghel1191@users.noreply.github.com>
2023-09-12 20:14:36 +0530

    my first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLoyout = ({children}: {children: React.ReactNode}) => {
+    return ( 
+        <div className="h-full flex items-center justify-center">
+            {children}
+        </div>
+     );
+}
+ 
+export default AuthLoyout;
